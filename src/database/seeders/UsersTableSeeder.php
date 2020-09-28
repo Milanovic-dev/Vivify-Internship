5507684301eb9ebb0b81df8454ae8f69905b8f32
Nikola Milanovic <nikolamilanovic21@gmail.com>
2020-09-28 15:40:55 +0200

    Added Migration and Seeder

---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        for($i = 1 ; $i < 10 ; $i++) {
+            DB::table('users')
+                ->insert([
+                    'name' => $faker->name,
+                    'email' => $faker->email,
+                    'address' => $faker->address,
+                    'password' => bycrypt('password')
+                ]);
+        }
+    }
+}
